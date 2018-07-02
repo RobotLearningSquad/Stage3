eb20e4046b21b67208cc7fc12e443dd01ab3f797
8652 <626531215@qq.com>
2018-07-03 01:11:00 +0800

    url router and function done.

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -6,7 +6,7 @@ import logging
 import os
 import time
 import bson
-from wordsRecog import recog
+import wordsRecog 
 # index/
 # command/        GET
   # put/          GET
@@ -27,92 +27,6 @@ from wordsRecog import recog
 #| 414 |	Request-URI Too Large | |
 #| 500 |	Internal Server Error | |
 
-app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config['SECRET_KEY'] = 'Session key'
-app.config['FLASK_DEBUG'] = 1
-app.config.update(
-    MONGO_HOST='localhost',
-    MONGO_PORT = 27017,
-    MONGO_USERNAME='tempo',
-    MONGO_PASSWORD='11111111',
-    MONGO_DBNAME='frage_DB'
-)
-mongo = PyMongo(app)
-
-class  Fridge(object):
-    def __init__(self, food, num, unit,date, leftHour):
-        self.food = food
-        self.num = num
-        self.unit = unit
-        self.date = date
-        self.leftHour = leftHour
-    
-# item can be None
-# return food List        
-def queryDB(userid, item):
-    foodlist = []
-    if userid is not None:
-        if item is not None:
-            frage = mongo.db.frages.find({'userid':userid, 'food':item})
-        else:
-            frage = mongo.db.frages.find({'userid':userid})
-        for food in frage:
-            food.pop('_id')
-            foodlist.append(food)
-    return foodlist
-
-# item can be None
-# return delete item count
-def deleteDB(userid, item):
-    if userid is not None:
-        if item is not None:
-            res = mongo.db.frages.delete_one({'userid': userid, 'food':item})
-            return res.deleted_count
-        else:
-            res = mongo.db.frages.delete_many({'userid':userid})
-            return res.deleted_count
-
-# if already have same (userid, food) do changeDB
-# do not return 
-def addDB(userid, food, num, unit, date, dateLeft):
-    frage = {'userid':userid, 'food':food, 'num':int(num), 'unit':unit, 'date':date, 'dateLeft':int(dateLeft)}
-    res = queryDB(userid, food)
-    if res is not None:
-        changeDB(userid, food, num, True)
-    else:
-        mongo.db.frages.insert_one(frage)
-
-# flag = true add, flag = false minus
-# if find and change return true else return false
-def changeDB(userid, item, num, flag):
-    if flag is True:
-        res = mongo.db.frages.update_one({'userid': userid}, {'$inc': {'num': num}})
-    else:
-        num = 0 - num
-        res = mongo.db.frages.update_one({'userid': userid}, {'$inc': {'num': num}})
-    if res.matched_count is 0:
-        return False
-    else:
-        return True
-
-class InvalidUsage(Exception):
-    status_code = 400
- 
-    def __init__(self, message, status_code=400):
-        Exception.__init__(self)
-        self.message = message
-        self.status_code = status_code
- 
-@app.errorhandler(InvalidUsage)
-def invalid_usage(error):
-    response = make_response(error.message)
-    response.status_code = error.status_code
-    return response
-
-@app.route('/', methods=['post', 'get'])
-@app.route('/index', methods=['[post', 'get'])
-def index():
     #debug
     #ticks = time.time()
     #localtime = time.localtime(time.time())
@@ -142,32 +56,150 @@ def index():
 
     #res = queryDB(userid, None)
     #print res
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'Session key'
+app.config['FLASK_DEBUG'] = 1
+app.config.update(
+    MONGO_HOST='localhost',
+    MONGO_PORT = 27017,
+    MONGO_USERNAME='tempo',
+    MONGO_PASSWORD='11111111',
+    MONGO_DBNAME='frage_DB'
+)
+mongo = PyMongo(app)
+
+class  Fridge(object):
+    def __init__(self, food, num, unit,date, leftHour):
+        self.food = food
+        self.num = num
+        self.unit = unit
+        self.date = date
+        self.leftHour = leftHour
     
-    
+# item can be None
+# return food List
+def queryDB(userid, item):
+    foodlist = []
+    if userid is not None:
+        if item is not None:
+            frage = mongo.db.frages.find({'userid':userid, 'food':item})
+        else:
+            frage = mongo.db.frages.find({'userid':userid})
+        for food in frage:
+            food.pop('_id')
+            foodlist.append(food)
+    return foodlist
+
+# item can be None
+# return delete item count
+def deleteDB(userid, item):
+    if userid is not None:
+        if item is not None:
+            res = mongo.db.frages.delete_one({'userid': userid, 'food':item})
+            return res.deleted_count
+        else:
+            res = mongo.db.frages.delete_many({'userid':userid})
+            return res.deleted_count
+
+# if already have same (userid, food) do changeDB
+# return type of "change" or "add"
+def addDB(userid, food, num, unit, date, dateLeft):
+    frage = {'userid':userid, 'food':food, 'num':int(num), 'unit':unit, 'date':date, 'dateLeft':int(dateLeft)}
+    res = queryDB(userid, food)
+    print len(res)
+    if len(res) > 0:
+        changeDB(userid, food, num, True)
+        return "change"
+    else:
+        mongo.db.frages.insert_one(frage)
+        return "add"
+
+# flag = true add, flag = false minus
+# if find and change return true else return false
+def changeDB(userid, item, num, flag):
+    if flag is True:
+        res = mongo.db.frages.update_one({'userid': userid}, {'$inc': {'num': num}})
+    else:
+        num = 0 - num
+        res = mongo.db.frages.update_one({'userid': userid}, {'$inc': {'num': num}})
+    if res.matched_count is 0:
+        return False
+    else:
+        return True
+
+class InvalidUsage(Exception):
+    status_code = 400
+ 
+    def __init__(self, message, status_code=400):
+        Exception.__init__(self)
+        self.message = message
+        self.status_code = status_code
+ 
+@app.errorhandler(InvalidUsage)
+def invalid_usage(error):
+    response = make_response(error.message)
+    response.status_code = error.status_code
+    return response
+
+@app.route('/', methods=['post', 'get'])
+def timePage():
+    #debug
+    date = time.time()
+    localtime = time.localtime(time.time())
+    userid='127.0.0.1'
+    food= 'fuck'
+    num= 3
+    unit='one'
+    dateLeft= 10
+    frage1 = {'userid':userid, 'food':food, 'num':int(num), 'unit':unit, 'date':date, 'dateLeft':int(dateLeft)}
+    #frage2 = {'userid':'0.0.0.1', 'food':food, 'num':int(num), 'unit':unit, 'date':date, 'dateLeft':int(dateLeft)}
+    #frage3 = {'userid':userid, 'food':'toufu', 'num':int(num), 'unit':unit, 'date':date, 'dateLeft':int(dateLeft)}
+    res = addDB(userid,food,num,unit,date,dateLeft)
+    print res
+    #mongo.db.frages.insert_one(frage3)
+    #res = queryDB(userid, food)
+    #print res
+
+    #res = changeDB(userid, food, 5, True)
+    #print res
+    return render_template('U-ROBO-1.html')
+
+@app.route('/index', methods=['[post', 'get'])
+def index():
     # set user cookies
     userid = request.cookies.get('userid')
     if userid is None :
         userid = request.remote_addr
         print 'no cookie'
+    resp = make_response(render_template('U-ROBO-2.html'))
+    resp.set_cookie('userid', userid)
+    return resp
+
+@app.route('/index/request', methods=['get'])
+def indexRequire():
+    userid = request.cookies.get('userid')
+    if userid is None :
+        print 'no cookie'
+        raise InvalidUsage('user doesnt exit', 403)
     else:
         # Query and find items and show
-        print 'tbc...'
-    resp = make_response(render_template('index.html'))
-    resp.set_cookie('userid', userid)
-    print userid
+        foodList = queryDB(userid, None)
+        print foodList
+    resp = make_response(jsonify(foodList))
+    test = jsonify(foodList)
     return resp
 
 @app.route('/command', methods=['post', 'get'])
 def commandJump(jump=0):
     if jump is 0:
-        #wait to be replaced by ('/command.html')
-        return render_template('')
+        return render_template('U-ROBO-2.html')
     if jump is 1:
-        return render_template('')
+        return render_template('U-ROBO-4.html')
     if jump is 2:
-        return render_template('')
-    #wait to be replaced by ('/command.html') 
-    return render_template('greet.html', name=None)
+        return render_template('U-ROBO-5.html')
+    return render_template('U-ROBO-3.html')
 
 ALLOWED_EXTENSIONS = set(['wav'])
 UPLOAD_FOLDER = './static/audio'
@@ -182,7 +214,7 @@ def allowed_file(filename):
 def putIn():
     userid = request.cookies.get('userid')
     #wait to be replaced by ('/command/put.html') 
-    resp = make_response(render_template('upload.html'))
+    resp = make_response(render_template('U-ROBO-4.html'))
     resp.status_code=200
     if userid is None:
         # no cookies Forbidden
@@ -195,12 +227,14 @@ def putIn():
             basepath = os.path.abspath(UPLOAD_FOLDER)
             print os.path.join(basepath, filename)
             file.save(os.path.join(basepath, filename))
-            result = recog(os.path.join(UPLOAD_FOLDER, filename))
+            #result = recog(os.path.join(UPLOAD_FOLDER, filename))
+            result = {'userid':userid, 'food':'apple', 'num':int(5), 'unit':'', 'date':'', 'dateLeft':int(24)}
             print result
             # split into words
 
             # save in DB
-
+            ticks = time.time()
+            addDB(userid,result['food'],result['num'],"",ticks,result['dateLeft'])
             return resp
         else:    
             # illegal
@@ -213,11 +247,11 @@ def putIn():
 def page_not_found(error):
     return render_template('404.html')
 
-@app.route('/command/change', methods=['post'])
-def changeIn():
+@app.route('/command/addFood', methods=['post'])
+def addFood():
     userid = request.cookies.get('userid')
     #wait to be replaced by ('/command/change.html') 
-    resp = make_response(render_template('upload.html'))
+    resp = make_response(render_template('U-ROBO-4.html'))
     resp.status_code=200
     if userid is None:
         # no cookies Forbidden
@@ -230,12 +264,13 @@ def changeIn():
             basepath = os.path.abspath(UPLOAD_FOLDER)
             print os.path.join(basepath, filename)
             file.save(os.path.join(basepath, filename))
-            result = recog(os.path.join(UPLOAD_FOLDER, filename))
+            #result = recog(os.path.join(UPLOAD_FOLDER, filename))
+            result = {'userid':userid, 'food':'apple', 'num':int(5), 'unit':'', 'date':'', 'dateLeft':int(24)}
             print result
             # split into words
 
             # save in DB
-
+            changeDB(userid,result['food'],result['num'],True)
             return resp
         else:
             # illegal
@@ -243,6 +278,39 @@ def changeIn():
     else:
         raise InvalidUsage('wrong instruction', 400)
     return resp
+
+@app.route('/command/getFood', methods=['post'])
+def getFood():
+    userid = request.cookies.get('userid')
+    #wait to be replaced by ('/command/change.html') 
+    resp = make_response(render_template('U-ROBO-5.html'))
+    resp.status_code=200
+    if userid is None:
+        # no cookies Forbidden
+        return redirect(url_for('/'), 403)
+
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            basepath = os.path.abspath(UPLOAD_FOLDER)
+            print os.path.join(basepath, filename)
+            file.save(os.path.join(basepath, filename))
+            #result = wordsRecog.main(os.path.join(UPLOAD_FOLDER, filename))
+            result = {'userid':userid, 'food':'apple', 'num':int(5), 'unit':'', 'date':'', 'dateLeft':int(24)}
+            print result
+            # split into words
+
+            # save in DB
+            changeDB(userid,result['food'],result['num'], False)
+            return resp
+        else:
+            # illegal
+            raise InvalidUsage('illegal file input', 403)
+    else:
+        raise InvalidUsage('wrong instruction', 400)
+    return resp
+
 
 @app.route('/command/show', methods=['get'])
 def show():
@@ -255,7 +323,7 @@ def show():
         return redirect(url_for('/'), 403)
     
     # set DB items into response
-    return resp
+    return render_template('U-ROBO-2.html')
 
 
 
